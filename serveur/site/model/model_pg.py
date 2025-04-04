890a258b68248a56f83cf18a7b1ea358be352734
p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-04-04 11:37:21 +0200

    modele

--- serveur/site/model/model_pg.py
+++ serveur/site/model/model_pg.py
@@ -3,10 +3,23 @@ from psycopg import sql
 from psycopg.rows import dict_row
 from logzero import logger
 
-def execute_select_query(connexion, query, params=[]):
+def execute_select_query(connexion, query :str, params :list=[]) -> list[dict]|None:
     """
     Méthode générique pour exécuter une requête SELECT (qui peut retourner plusieurs instances).
     Utilisée par des fonctions plus spécifiques.
+
+    Paramètres
+    ----------
+    connexion : 
+        Connexion à la base de donnée.
+    query : str like
+        Requête pour le SELECT.
+    params : list
+        Paramètres à ajouter à la requête.
+
+    Renvoie
+    -------
+    Une liste de dictionnaires. Chaque dictionnaire correspond à une ligne.
     """
     with connexion.cursor() as cursor:
         try:
@@ -18,10 +31,23 @@ def execute_select_query(connexion, query, params=[]):
             logger.error(e)
     return None
 
-def execute_other_query(connexion, query, params=[]):
+def execute_other_query(connexion, query :str, params :list=[]) -> int|None:
     """
     Méthode générique pour exécuter une requête INSERT, UPDATE, DELETE.
     Utilisée par des fonctions plus spécifiques.
+    
+    Paramètres
+    ----------
+    connexion : 
+        Connexion à la base de donnée.
+    query : str like
+        Requête à exécuter.
+    params : list
+        Paramètres à ajouter à la requête.
+
+    Renvoie
+    -------
+    Le nombre d'enregistrements.
     """
     with connexion.cursor() as cursor:
         try:
@@ -32,38 +58,103 @@ def execute_other_query(connexion, query, params=[]):
             logger.error(e)
     return None
 
-def get_instances(connexion, nom_table):
+def get_instances(connexion, nom_table :str) -> list[dict]|None:
     """
-    Retourne les instances de la table nom_table
-    String nom_table : nom de la table
+    Retourne les instances de la table 'nom_table'.
+    
+    Paramètres
+    ----------
+    connexion : 
+        Connexion à la base de donnée.
+    nom_table : str
+        Nom de la table.
+
+    Renvoie
+    -------
+    Les enregistrements de la table 'nom_table'.
     """
-    query = sql.SQL('SELECT * FROM {table}').format(table=sql.Identifier(nom_table), )
+    query = sql.SQL('SELECT * FROM {table}').format(table=sql.Identifier(nom_table))
     return execute_select_query(connexion, query)
 
-def count_instances(connexion, nom_table):
+def count_instances(connexion, nom_table :str) -> int:
     """
-    Retourne le nombre d'instances de la table nom_table
-    String nom_table : nom de la table
+    Retourne le nombre d'instances de la table 'nom_table'.
+    
+    Paramètres
+    ----------
+    connexion : 
+        Connexion à la base de donnée.
+    nom_table : str
+        Nom de la table.
+
+    Renvoie
+    -------
+    Le nombre d'enregistrements de la table 'nom_table'.
     """
     query = sql.SQL('SELECT COUNT(*) AS nb FROM {table}').format(table=sql.Identifier(nom_table))
-    return execute_select_query(connexion, query)
+    nb = execute_select_query(connexion, query)
+    if nb is None: return 0
+    return nb[0]['nb']
 
 
 
-def get_recipe(connexion, id_recette):
+def get_img_tuile(connexion, id_tuile :int) -> str:
     """
-    Retourne les informations concernant la recette identifiée par id_recette
-    """
-    query = 'SELECT * FROM recette WHERE id_recette=%s'
-    return execute_select_query(connexion, query, [id_recette])
+    Retourne l'image associée à la tuile d'identifiant 'id_tuile'
 
-def get_steps_recipe(connexion, id_recette):
+    Paramètres
+    ----------
+    connexion : 
+        Connexion à la base de donnée.
+    id_tuile : int
+        Identifiant de la tuile.
+
+    Renvoie
+    -------
+    Nom de l'image.
     """
-    Retourne les étapes de la recette recette id_recette
-    Integer id_recette : identifiant de la recette
+    query = 'SELECT chemin_texture FROM tuile WHERE id_tuile=%s'
+    image = execute_select_query(connexion, query, [id_tuile])
+    if image is None: return None
+    return image[0]['chemin_texture']
+
+def get_nb_element(connexion, id_tuile :int) -> int:
+    """
+    Retourne le nombre d'éléments présents sur la tuile jeu d'identifiant 'id_tuile'
+    
+    Paramètres
+    ----------
+    connexion : 
+        Connexion à la base de donnée.
+    id_tuile : int
+        Identifiant de la tuile jeu.
+
+    Renvoie
+    -------
+    Nombre d'éléments présents sur la tuile.
+    """
+    query = 'SELECT SUM(nombre) AS nb FROM contient_element WHERE id_tuile=%s '
+    nb = execute_select_query(connexion, query, [id_tuile])
+    if nb is None: return None
+    return nb[0]['nb']
+
+def get_element_simple(connexion, nom_element :str) -> str:
+    """
+    Retourne l'image de la tuile possédant seulement un élément 'nom_element'
+    
+    Paramètres
+    ----------
+    connexion : 
+        Connexion à la base de donnée.
+    nom_element : str
+        Nom de l'élément à afficher.
+
+    Renvoie
+    -------
+    Nom de l'image correspondant à la tuile souhaitée.
     """
     query = 'SELECT * FROM etape WHERE id_recette=%s ORDER BY numero'
-    return execute_select_query(connexion, query, [id_recette])
+    return execute_select_query(connexion, query, [nom_element])
 
 def is_existing_recipe(connexion, nom_recette):
     """
@@ -101,6 +192,3 @@ def get_table_like(connexion, nom_table, like_pattern):
         sql.Placeholder())
     #    like_pattern=sql.Placeholder(name=like_pattern))
     return execute_select_query(connexion, query, [motif])
-
-
-
