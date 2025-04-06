c99adc9038285027ebebcd237418d6546a8477b4
p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-04-06 21:17:58 +0200

    avancées de parties

--- serveur/site/controleurs/parties.py
+++ serveur/site/controleurs/parties.py
@@ -1,4 +1,12 @@
 from model.model_pg import nouvelle_partie
 
-if POST and 'taille_grille' in POST and 'difficulté' in POST:
-	nouvelle_partie(SESSION['CONNEXION'], POST['taille_grille'][0], POST['difficulté'][0], SESSION["joueur_actif"]["id_joueur"])
+
+if POST:
+	if 'action' in POST and POST["action"][0] == "nouvelle_partie":
+		REQUEST_VARS['AFFICHAGE'] = "FORMULAIRE"
+	else:
+		REQUEST_VARS['AFFICHAGE'] = "DEFAUT"
+	if 'taille_grille' in POST and 'difficulté' in POST:
+		nouvelle_partie(SESSION['CONNEXION'], POST['taille_grille'][0], POST['difficulté'][0], SESSION["joueur_actif"]["id_joueur"])
+else:
+	REQUEST_VARS['AFFICHAGE'] = "DEFAUT"
