f5947d84156cfbf3e6a6b1f73feb0cca4c90f738
p2306662 <p2306662@etu.univ-lyon1.fr>
2025-04-06 18:23:47 +0200

    fix: bug lorsqu'on sélectionne un joueur

--- serveur/site/controleurs/accueil.py
+++ serveur/site/controleurs/accueil.py
@@ -1,6 +1,6 @@
 from model.model_pg import get_instances, count_instances, parties_terminees, nb_parties, get_infos_joueur
 
-if POST and 'id_joueur' in POST: SESSION['joueur_actif'] = get_infos_joueur(SESSION['CONNEXION'], POST['id_joueur'])
+if POST and 'id_joueur' in POST: SESSION['joueur_actif'] = get_infos_joueur(SESSION['CONNEXION'], POST['id_joueur'][0])
 
 REQUEST_VARS['joueurs'] = (count_instances(SESSION['CONNEXION'], "joueur"), get_instances(SESSION['CONNEXION'], "joueur"))
 REQUEST_VARS['parties_terminées'] = parties_terminees(SESSION['CONNEXION'])
