94a771f3e75857c9f88bfbf31799d2b8d5ced405
p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-04-14 18:19:58 +0200

    schéma E/A

--- serveur/site/controleurs/jeu.py
+++ serveur/site/controleurs/jeu.py
@@ -4,15 +4,13 @@ if 'taille_grille' in POST and 'difficulté' in POST:
 	id_partie = nouvelle_partie(SESSION['CONNEXION'], POST['taille_grille'][0], POST['difficulté'][0], SESSION["joueur_actif"][0]["id_joueur"])
 	SESSION['partie_en_cours'] = get_infos_partie(SESSION['CONNEXION'], id_partie)
 
-REQUEST_VARS['phase'] = "joue_carte" # 'defausse_carte'
-
 REQUEST_VARS['taille_grille'] = SESSION['partie_en_cours']['taille_grille']
 
 if (grille_remplie(SESSION['CONNEXION'], SESSION['partie_en_cours']['id_partie'])) : REQUEST_VARS['phase'] = 'resultats'
 else :
     pass
 
-
+REQUEST_VARS['phase'] = "joue_carte" # 'defausse_carte'
 REQUEST_VARS['grille'] = [
 	[None       , 'tc111.png', 'tc111.png', 'tc111.png', 'tc111.png'],
 	['tc111.png', 'tj42.png' , None       , 'tj42.png' , 'tj42.png' ],
