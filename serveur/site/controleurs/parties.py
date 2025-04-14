be512980c4df46f79bdcba47a4926b68dd8e5eea
p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-04-14 17:43:06 +0200

    parties

--- serveur/site/controleurs/parties.py
+++ serveur/site/controleurs/parties.py
@@ -4,12 +4,8 @@ if "joueur_actif" in SESSION:
 	REQUEST_VARS['parties'] = parties_en_cours(SESSION['CONNEXION'], SESSION["joueur_actif"][0]["id_joueur"])
 	REQUEST_VARS['AFFICHAGE'] = "DEFAUT"
 	
-	if POST and 'action' in POST:
-		if POST["action"][0] == "nouvelle_partie":
-			REQUEST_VARS['AFFICHAGE'] = "FORMULAIRE"
-		elif 'taille_grille' in POST and 'difficulté' in POST:
-			id_partie = nouvelle_partie(SESSION['CONNEXION'], POST['taille_grille'][0], POST['difficulté'][0], SESSION["joueur_actif"][0]["id_joueur"])
-			SESSION['partie_en_cours'] = get_infos_partie(SESSION['CONNEXION'], id_partie)
+	if POST and 'action' in POST and POST["action"][0] == "nouvelle_partie":
+		REQUEST_VARS['AFFICHAGE'] = "FORMULAIRE"
 		
 else:
 	REQUEST_VARS['AFFICHAGE'] = "ACCUEIL"
