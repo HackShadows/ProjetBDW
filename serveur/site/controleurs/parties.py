15c3e6e58c79663daaa63d281181d29ad873264c
p2303380 <marius.ciserane@etu.univ-lyon1.fr>
2025-04-14 16:52:23 +0200

    parties

--- serveur/site/controleurs/parties.py
+++ serveur/site/controleurs/parties.py
@@ -2,16 +2,13 @@ from model.model_pg import nouvelle_partie, parties_en_cours
 
 if "joueur_actif" in SESSION:
 	REQUEST_VARS['parties'] = parties_en_cours(SESSION['CONNEXION'], SESSION["joueur_actif"][0]["id_joueur"])
-else:
-	pass
-
-if POST and 'action' in POST:
-	if POST["action"][0] == "nouvelle_partie":
-		REQUEST_VARS['AFFICHAGE'] = "FORMULAIRE"
-	elif 'taille_grille' in POST and 'difficulté' in POST:
-		nouvelle_partie(SESSION['CONNEXION'], POST['taille_grille'][0], POST['difficulté'][0], SESSION["joueur_actif"][0]["id_joueur"])
-		REQUEST_VARS['AFFICHAGE'] = "DEFAUT"
-	else:
-		REQUEST_VARS['AFFICHAGE'] = "DEFAUT"
-else:
 	REQUEST_VARS['AFFICHAGE'] = "DEFAUT"
+	
+	if POST and 'action' in POST:
+		if POST["action"][0] == "nouvelle_partie":
+			REQUEST_VARS['AFFICHAGE'] = "FORMULAIRE"
+		elif 'taille_grille' in POST and 'difficulté' in POST:
+			nouvelle_partie(SESSION['CONNEXION'], POST['taille_grille'][0], POST['difficulté'][0], SESSION["joueur_actif"][0]["id_joueur"])
+		
+else:
+	REQUEST_VARS['AFFICHAGE'] = "ACCUEIL"
